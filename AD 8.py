f6136dae77c5097447ad30687ca58a1af906265c
Shahnavaz Virani <workshahnavaz@gmail.com>
2024-07-06 12:20:06 +0530

    Add files via upload

---
+++ AD 8.py
@@ -0,0 +1,58 @@
+# Inheritence
+
+# class A:
+#     def sagar1(self):
+#         print("hello1")
+#     def sagar2(self):
+#         print("hello2")
+# class B(A):
+#     def fech3(self):
+#         print("Hello3")
+#     def fech4(self):
+#         print("Hello4")
+    
+# a1 = B()
+
+# a1.sagar1()
+# a1.sagar2()
+# a1.fech3()
+# a1.fech4()
+
+# Itretor
+
+# Hello = [1,2,3,4,5]
+# its = iter(Hello)
+
+# print(its.__next__())
+# print(its.__next__())
+# print(its.__next__())
+
+# print(next(its))
+
+# Generatours
+
+# def Hello():
+#     yield 5
+
+# value = Hello()
+# print(value.__next__())
+
+# python Math
+
+# x = min(1,3,44,5)
+# print(x)
+
+# s = max(2,3,4,5)
+# print(s)
+
+
+# x= abs(-4.34)
+# print(x)
+
+# y = pow(2,3)
+# print(y)
+
+# import math
+
+# x = math.pi
+# print(x)
